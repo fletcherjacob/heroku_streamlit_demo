62032b7abf8af25e855fd1ccffeed6f33c826d06
Jacob Fletcher <fletcher.c.jacob@gmail.com>
2023-03-09 23:34:00 -0500

    Create

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit
+
+# Configs
+st.set_page_config(
+    page_title="Smart Rates Simulation ",
+    layout='wide'
+)
+
+'''
+# Smart Rates Simulation 
+-An Interactive Dashboard
+'''
+# Create a file uploader and read the uploaded file
