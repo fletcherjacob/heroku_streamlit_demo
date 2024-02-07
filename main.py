cabe5a54c9f40415e036c6b7de7db89650edc0b2
fletcherjacob <fletcher.c.jacob@gmail.com>
2024-02-07 14:14:13 -0500

    update doc

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
 import streamlit as st
 
+
+# Change to for your application
 st.title("Streamlit Password Testing")
+
+
+# Main Password Checking Function
 def check_password():
+    """While True"""
+
     def login_form():
         with st.form("Credentials"):
             st.text_input("Username", key="username")
@@ -10,17 +17,16 @@ def check_password():
             st.form_submit_button("Log in", on_click=password_entered)
 
     def password_entered():
+        # Check Heroku Vars based on username
         if st.session_state["password"] == os.getenv(st.session_state["username"]):
             st.session_state["password_correct"] = True
             del st.session_state["password"]
-            del st.session_state["username"] # Don't store the password.
+            del st.session_state["username"]  # Don't store the password.
         else:
             st.session_state["password_correct"] = False
 
-
     if st.session_state.get("password_correct", False):
         return True
-
 
     login_form()
     if "password_correct" in st.session_state:
@@ -31,8 +37,9 @@ def check_password():
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
+# Input Rest Of Streamlit Code Below
 
-
+# Simple Exponent Calculator
 x = st.number_input("Insert a number", min_value=0, max_value=100, format="%i", step=1)
 y = st.number_input("Exponent", min_value=0, max_value=100, format="%i", step=1)
 z = x**y
